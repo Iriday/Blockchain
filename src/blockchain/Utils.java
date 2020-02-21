434169b0b74fe3c908c2d0464d06c228365573a9
Igor <GI_Something@protonmail.com>
2020-02-21 17:51:59 +0200

    feat: Add method to check if hash starts with specified num of zeros

--- src/blockchain/Utils.java
+++ src/blockchain/Utils.java
@@ -22,4 +22,21 @@ public class Utils {
             throw new RuntimeException(e);
         }
     }
+
+    /**
+     * Checks if this string starts with specified number of zeros.
+     * @param str the string to be checked.
+     * @param numOfZeros the number of leading zeros.
+     * @return true if str starts with specified number of zeros; false if str has less or more leading zeros.
+     * @throws IllegalArgumentException if numOfZeros < 0 || numOfZeros > str.length || str.isEmpty.
+     */
+    public static boolean startsWithZeros(String str, int numOfZeros) {
+        if (numOfZeros < 0 || numOfZeros > str.length() || str.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        for (int i = 0; i < numOfZeros; i++) {
+            if (str.charAt(i) != '0') return false;
+        }
+        return str.charAt(numOfZeros) != '0';
+    }
 }
