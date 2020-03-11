2e7a12d2d56f8bcf59a057847a251ff7ee82ca37
Igor <GI_Something@protonmail.com>
2020-03-11 11:16:25 +0200

    feat: Sign Message.message, Message stores signature, public key

--- src/blockchain/BlockData.java
+++ src/blockchain/BlockData.java
@@ -1,9 +1,14 @@
 package blockchain;
 
 import java.io.Serializable;
+import java.security.PublicKey;
 
 public interface BlockData extends Serializable {
     long getId();
 
     String getData();
+
+    byte[] getSignature();
+
+    PublicKey getPublicKey();
 }
--- src/blockchain/Message.java
+++ src/blockchain/Message.java
@@ -1,15 +1,26 @@
 package blockchain;
 
 import java.io.Serializable;
+import java.security.KeyPair;
+import java.security.PublicKey;
 
 public class Message implements BlockData, Serializable {
 
     private final String message;
     private final long id;
+    private final byte[] signature;
+    private final PublicKey publicKey;
 
     public Message(String message, long id) {
         this.message = message;
         this.id = id;
+        try {
+            KeyPair keyPair = SignatureUtils.generateKeys();
+            this.publicKey = keyPair.getPublic();
+            this.signature = SignatureUtils.createSignature((message + id), keyPair.getPrivate());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
@@ -23,6 +34,15 @@ public class Message implements BlockData, Serializable {
         return id;
     }
 
+    @Override
+    public byte[] getSignature() {
+        return signature;
+    }
+
+    @Override
+    public PublicKey getPublicKey() {
+        return publicKey;
+    }
 
     public static Message getEmptyData() {
         return new Message("no data", 0);
