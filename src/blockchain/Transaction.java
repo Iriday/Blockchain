6b895a54bee160731ece6221dd9dd8c04d76a6d7
Igor <GI_Something@protonmail.com>
2020-03-16 13:36:27 +0200

    refactor: Change separator from " " to "\n" (Transaction constructor)
    Transaction.getEmptyData is static.
    Removed "\n" before "No transactions".

--- src/blockchain/Transaction.java
+++ src/blockchain/Transaction.java
@@ -6,16 +6,16 @@ import java.security.KeyPair;
 public class Transaction extends BlockData implements Serializable {
 
     public Transaction(long id, String sender, long virtualCoins, String receiver, KeyPair keyPair) {
-        super(sender + " " + virtualCoins + " " + receiver, id, keyPair);
+        super(sender + "\n" + virtualCoins + "\n" + receiver, id, keyPair);
     }
 
-    public Transaction getEmptyData() {
+    public static Transaction getEmptyData() {
         KeyPair keyPair;
         try {
             keyPair = SignatureUtils.generateKeys();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-        return new Transaction(0, "\nNo transactions", -1, "No transactions", keyPair);
+        return new Transaction(0, "No transactions", -1, "No transactions", keyPair);
     }
 }
